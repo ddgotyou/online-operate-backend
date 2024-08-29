1f055e0799a3719759704a52d6f6f59d4c25eb10
yihan_dai <yihan_dai@intsig.net>
2024-08-29 20:10:28 +0800

    数据库连接

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+const {MongoClient} = require('mongodb');
+const uri = "mongodb+srv://root:020107@cluster0.l2pf5.mongodb.net/";
+const client = new MongoClient(uri);
+async function fetchUser() {
+  try {
+    const database = client.db('operate-development');
+    const users = database.collection('user');
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { user_name: 'ddgotyou' };
+    const user = await users.findOne(query);
+    return user
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+
+  }
+}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/getuser', function(req, res, next) {
+  const userInfo= fetchUser()
+  console.log(userInfo)
+  res.send(userInfo)
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
