9a719de73c56faf0017ecae60ccd1f343f1eced2
yihan_dai <yihan_dai@intsig.net>
2024-09-01 17:11:49 +0800

    简易登陆

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ var router = express.Router();
 const { MongoClient } = require("mongodb");
 const uri = "mongodb+srv://root:020107@cluster0.l2pf5.mongodb.net/";
 const client = new MongoClient(uri);
-async function fetchUser() {
+async function fetchUser(params) {
   try {
     const database = client.db("operate-development");
     const users = database.collection("user");
     // Query for a movie that has the title 'Back to the Future'
-    const query = { user_name: "ddgotyou" };
+    const query = { user_name: params.user_name };
     const user = await users.findOne(query);
     console.log("uuu", user);
     return user;
@@ -20,8 +20,10 @@ async function fetchUser() {
 
 /* GET home page. */
 router.get("/getuser", async function (req, res, next) {
-  const userInfo = await fetchUser();
-  console.log(userInfo);
+  const params = req.query;
+  console.log(req.query);
+  const userInfo = await fetchUser(params);
+
   res.send(userInfo);
   // res.render('index', { title: 'Express' });
 });
