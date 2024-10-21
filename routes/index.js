4e37eef9548969c1d708ed4a7b637b9cfc9ca17d
yihan_dai <yihan_dai@intsig.net>
2024-10-21 19:31:53 +0800

    完成用户的注册模块

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const { usersCollection } = require("../mongodb/open");
 async function fetchUser(params) {
   try {
     // Query for a movie that has the title 'Back to the Future'
-    const query = { user_name: params.user_name };
+    const query = { user_name: params.user_name,password:params.password };
     const user = await usersCollection.findOne(query);
     if (user) {
       //如果找到了,jwt生成token
@@ -35,6 +35,23 @@ router.get("/getUser", async function (req, res, next) {
 router.get("/getUserList", async function (req, res, next) {
   const userList = await usersCollection.find().toArray();
   res.send(userList);
+});
+
+/* 注册用户 */
+router.post("/register", function (req, res, next) {
+  const { user_name, password } = req.body
+  //根据文档id 获取文档信息
+  usersCollection
+    .insertOne({ user_name, password})
+    .then((msg) => {
+      console.log("用户创建成功", msg);
+      res.send({ code: 200, msg: "用户创建成功" });
+    })
+    .catch((err) => {
+      console.log("更新失败", err);
+      res.send({ code: 500, msg: "用户创建失败" });
+    });
+  
 });
 
 module.exports = router;
